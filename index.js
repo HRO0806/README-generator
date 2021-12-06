1f29be55affd55eeeef270d08f5bba0cd7082ecd
Henry <henryolson@icloud.com>
2021-12-06 11:04:52 -0600

    Now generates README file

---
+++ index.js
@@ -0,0 +1,33 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+/*const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();*/
+
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile('Develop/dist/Generated.md', fileContent, err => {
+        if (err) {
+          reject(err);
+          return;
+        }
+  
+        resolve({
+          ok: true,
+          message: 'File created!'
+        });
+      });
+    });
+  };
+
+writeFile(test);
