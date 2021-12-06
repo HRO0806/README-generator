1ea8041eee92d62e8fea90fa9b593d8656f5f07b
Henry <henryolson@icloud.com>
2021-12-06 11:06:34 -0600

    updated the name of a file

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const fs = require('fs');
 
 const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
-      fs.writeFile('Develop/dist/Generated.md', fileContent, err => {
+      fs.writeFile('develop/dist/Generated.md', fileContent, err => {
         if (err) {
           reject(err);
           return;
